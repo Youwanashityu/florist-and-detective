0bf55ba691ad71f0d6a6a0bc4fa12d445f70b356
陽 <youwanashityu@gmail.com>
2026-03-16 22:21:11 +0900

    [Add/Update]オーディオがゲーム画面の方でへんこうできるようにしました　

---
+++ Assets/Script/SEChange.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+/// <summary>
+/// ボタンやイベントに合わせてSEを再生するコンポーネント。
+/// インスペクターでSE名を指定し、PlaySE()を呼び出すだけで再生できます。
+/// </summary>
+public class SEChange : MonoBehaviour
+{
+    // -------------------------------------------------------
+    // インスペクター設定
+    // -------------------------------------------------------
+
+    [Header("再生するSE名（SoundManagerのSE一覧に登録した名前）")]
+    [SerializeField] private string seName;
+
+    // -------------------------------------------------------
+    // SE再生
+    // -------------------------------------------------------
+
+    /// <summary>
+    /// インスペクターで指定したSEをSoundManager経由で再生します。
+    /// ボタンのOnClickやAnimationEventから呼び出せます。
+    /// </summary>
+    public void PlaySE()
+    {
+        if (SoundManager.Instance == null)
+        {
+            Debug.LogWarning("[SEChange] SoundManager が見つかりません。");
+            return;
+        }
+
+        SoundManager.Instance.PlaySE(seName);
+    }
+}
